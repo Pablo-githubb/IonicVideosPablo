a2acaf767037fc2e51b40175b05b6d1224b9807a
Pablo-githubb <pablomaso@iesebre.com>
2026-05-22 22:42:25 +0200

    documentació

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,10 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true
+      },
+      '/storage': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
       }
     }
   },
